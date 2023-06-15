de156848719ff7eab828864ad903ed77c697e353
yournetf <60455790+yournetf@users.noreply.github.com>
2023-06-15 10:52:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("navBar").style.display = "flex";
+    document.getElementById("button").style.display = "none";
+}
+
+document.getElementById("button").onmouseover = function(){
+    
+}
+
+
