2ec63e986d22734b45888e8225748a40f15b80be
yournetf <60455790+yournetf@users.noreply.github.com>
2023-06-16 10:33:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-document.getElementById("button").onclick = function(){
-    document.getElementById("navBar").style.display = "flex";
-    document.getElementById("button").style.display = "none";
-}
 
-document.getElementById("button").onmouseover = function(){
+
+document.getElementById("beginButton").onclick = function(){
     
+    document.getElementById("beginButton").style.display = "none";
+    document.getElementById("projectButton").style.display = "block";
+    document.getElementById("contactButton").style.display = "block";
+  
 }
 
+let myDocument = document.documentElement;
+let btn = document.getElementById("beginButton");
+beginButton.addEventListener("click", ()=>{
+    if(btn.textContent == "Begin"){
+        if (myDocument.requestFullscreen) {
+            myDocument.requestFullscreen();
+        } 
+        else if (myDocument.msRequestFullscreen) {
+            myDocument.msRequestFullscreen();
+        } 
+        else if (myDocument.mozRequestFullScreen) {
+            myDocument.mozRequestFullScreen();
+        }
+        else if(myDocument.webkitRequestFullscreen) {
+            myDocument.webkitRequestFullscreen();
+        }
+    }
+    else{
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        else if(document.msexitFullscreen) {
+            document.msexitFullscreen();
+        }
+        else if(document.mozexitFullscreen) {
+            document.mozexitFullscreen();
+        }
+        else if(document.webkitexitFullscreen) {
+            document.webkitexitFullscreen();
+        }
+        btn.textContent = "Go Fullscreen";
+    }
+});
 
